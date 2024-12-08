c85b749238a9e7f0abefe2247a4077f9a7123aac
Tapan Sharma <tapansharma69@gmail.com>
2024-12-08 15:57:14 +0545

    install and configure sanctum

--- backend/config/app.php
+++ backend/config/app.php
@@ -101,7 +101,7 @@ return [
 
     'previous_keys' => [
         ...array_filter(
-            explode(',', env('APP_PREVIOUS_KEYS', ''))
+            explode(',', env('APP_PREVIOUS_KEYS', '')),
         ),
     ],
 
@@ -122,5 +122,7 @@ return [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
+
+    'frontend_url' => env('FRONTEND_URL', 'http://localhost:5173'),
 
 ];
